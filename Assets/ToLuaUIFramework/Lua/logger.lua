fd070311a8d1171fed150f84ad3fdd323d637022
[LO] <wqb55@qq.com>
2021-03-16 17:09:52 +0800

    update Assets/ToLuaUIFramework/Lua/logger.lua.

--- Assets/ToLuaUIFramework/Lua/logger.lua
+++ Assets/ToLuaUIFramework/Lua/logger.lua
@@ -14,9 +14,37 @@ local function isMetatable(data)
     return false
 end
 
-function dataToJson(data)
+local function isLuaClass(data)
+    if type(data) == "table" then
+        for key, value in pairs(data) do
+            if type(value) == "function" then
+                return true
+            end
+            if type(value) == "table" then
+                for k, v in pairs(value) do
+                    if type(v) == "function" then
+                        return true
+                    end
+                end
+            end
+        end
+    end
+    return false
+end
+
+function dataToJson(data, depth)
     if data == nil then
         return "nil"
+    end
+    depth = depth + 1
+    if depth > 8 then
+        return tostring(data)
+    end
+    if isLuaClass(data) then
+        if data.__cname then
+            return "class:(" .. data.__cname .. ")"
+        end
+        return tostring(data)
     end
     if type(data) == "table" or isMetatable(data) then
         local str = "{"
@@ -24,7 +52,7 @@ function dataToJson(data)
             if str ~= "{" then
                 str = str .. ","
             end
-            str = str .. '"' .. key .. '"' .. ":" .. dataToJson(value)
+            str = str .. '"' .. key .. '"' .. ":" .. dataToJson(value, depth)
         end
         return str .. "}"
     elseif type(data) == "string" then
@@ -42,7 +70,8 @@ local function _log(data, method, tag)
     if method == 3 then
         func = Debugger.LogError
     end
-    local str = dataToJson(data)
+    local depth = 1
+    local str = dataToJson(data, depth)
     if tag then
         func("WQB=> [" .. tag .. "]=> " .. str .. "\n" .. debug.traceback())
     else
